585168cd8074896f5bcd736913205659b06bb809
pugazhendhi <128356450+Pugazh-s7a@users.noreply.github.com>
2024-02-29 12:46:29 +0530

    Zip and Try-except

---
+++ pugazhendhi/test.py
@@ -0,0 +1,33 @@
+# <---ZIP of Python Code ---->
+# a = [i for i in range(0,10)]
+# b = [i for i in range(10,20)]
+# c = [i for i in range(20,30)]
+
+# for i,x in enumerate(zip(a,b,c)):
+#     merged = ' '.join(str(y) for y in x)
+#     print("index",i,"values",merged)
+
+
+# <---Declare Initial Variable---> 
+
+# def funtion(a,b,condition):
+#     sum = 0
+#     data = ''
+#     for i in range(1,10):
+#         if condition == 'sum':
+#             sum +=i
+#         elif condition == 'multiply':
+#             sum = i * i
+#     return sum
+
+# Try-Except Concept
+# try:
+# except IndexError: 
+# else:
+# finally:
+
+
+
+
+
+
